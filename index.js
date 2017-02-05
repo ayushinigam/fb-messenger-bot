06c96447a419986afd847211a7bc0d476258cdf8
ayushi-nigam <ayushi_nigam@mckinsey.com>
2017-02-05 22:22:41 +0530

    bot interactions added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const token = "EAACY4rRsG6cBAFoHDoodnFUj7pHDomrX8jNvZCIhin5UwiYWKHI0ZCaoFkYnRZAPTp6o9kaaiFGGgrCWxZAJiYELson4bGv1X1KEUSGwqqx1bYvKDe97gpBmlTZCdyxVwVCW7SulWQQZB1RZAo5FOMTnjOO10Ea5AwP64LlmbgZB8QZDZD"
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -25,6 +26,38 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
